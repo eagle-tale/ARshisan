09e418473d7ad67d0056144a45e837ce758bffb1
eagle-tale <43973322+eagle-tale@users.noreply.github.com>
2023-11-28 22:36:27 +0900

    video: { facingMode: "environment" }）を試す

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const video = document.getElementById('video');
 let contentWidth;
 let contentHeight;
 
-const media = navigator.mediaDevices.getUserMedia({ audio: false, video: { width: 640, height: 480 } })
+const media = navigator.mediaDevices.getUserMedia({ audio: false, video: { facingMode: "environment" } })
     .then((stream) => {
         video.srcObject = stream;
         video.onloadeddata = () => {
